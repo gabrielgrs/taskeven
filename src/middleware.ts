9844ae20a3011177275c944e6c1a899d92f5b9f8
Gabriel R <grxgabriel@gmail.com>
2024-03-07 11:06:52 -0300

    :bug: Fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,34 @@
 import { NextRequest, NextResponse } from 'next/server'
 
-export async function middleware(req: NextRequest) {
+function getToken(req: NextRequest) {
   const urlSearchParams = new URLSearchParams(req.nextUrl.search)
-  const tokenParam = urlSearchParams.get('token')
-  if (tokenParam) {
+  return urlSearchParams.get('token') || req.cookies.get('token')?.value
+}
+
+export async function middleware(req: NextRequest) {
+  const token = getToken(req)
+
+  if (req.url.includes('/logout')) {
     const response = NextResponse.redirect(new URL('/', req.url))
-    response.cookies.set('token', tokenParam)
+    response.cookies.delete('token')
+    return response
+  }
+
+  if (req.url.includes('/auth') && token) {
+    const response = NextResponse.redirect(new URL('/', req.url))
+    response.cookies.set('token', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV !== 'development',
+      sameSite: 'strict',
+      maxAge: 60 * 60 * 24 * 30,
+      path: '/',
+    })
     return response
   }
 
   const response = NextResponse.next()
-  const token = req.cookies.get('token')?.value
   if (token) response.cookies.set('token', token)
+
   return response
 }
 
