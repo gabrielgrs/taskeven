0beaa632811df67191cb95f0241c1046bc2cbe40
Gabriel Ribeiro <grxgabriel@gmail.com>
2024-12-05 15:59:36 -0300

    Make timeline page dynamic instead of static

--- src/app/(private)/timeline/page.tsx
+++ src/app/(private)/timeline/page.tsx
@@ -1,6 +1,8 @@
 import { getAuthenticatedUser } from '@/actions/auth'
 import { Template } from './template'
 
+export const dynamic = 'force-dynamic'
+
 export default async function Home() {
 	const [authUser, err] = await getAuthenticatedUser()
 	if (err) throw err
